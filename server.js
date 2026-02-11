84064628809036c97dd9c831e18fd882bc48583a
Yossi Bokor Bleile <yossi@yossi.eu>
2026-02-12 00:16:38 +0100

    Pesky pesky bugs

--- server.js
+++ server.js
@@ -6,19 +6,31 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const SITES = path.join(__dirname, "sites");
 
-// Subsites served under /toma, /correa, /files
-app.use("/toma", express.static(path.join(SITES, "toma.yossi.eu")));
-app.use("/correa", express.static(path.join(SITES, "correa.yossi.eu")));
-app.use("/files", express.static(path.join(SITES, "files.yossi.eu")));
-app.use("/files", serveIndex(path.join(SITES, "files.yossi.eu"), { icons: true }));
-
-// Main site at root
-app.use(express.static(path.join(SITES, "yossi.eu")));
-
-// Debug: show all headers
-app.use((req, res) => {
-  res.type("json").send(JSON.stringify(req.headers, null, 2));
+// Route subdomains to their site directories
+app.use((req, res, next) => {
+  const host = req.hostname;
+  const siteDir = path.join(SITES, host);
+  if (host !== "yossi.eu" && host.endsWith(".yossi.eu")) {
+    req.siteDir = siteDir;
+  }
+  next();
 });
+
+// File server with directory listing
+app.use((req, res, next) => {
+  if (req.hostname === "files.yossi.eu") {
+    express.static(path.join(SITES, "files.yossi.eu"))(req, res, () => {
+      serveIndex(path.join(SITES, "files.yossi.eu"), { icons: true })(req, res, next);
+    });
+  } else if (req.siteDir) {
+    express.static(req.siteDir)(req, res, next);
+  } else {
+    next();
+  }
+});
+
+// Main site at root (yossi.eu and preview URL fallback)
+app.use(express.static(path.join(SITES, "yossi.eu")));
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
