41308a80c9db07747b608b60397e684dc01a83e8
Yossi Bokor Bleile <yossi@yossi.eu>
2026-02-11 23:59:13 +0100

    Pesky bugs

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const vhost = require("vhost");
 const serveIndex = require("serve-index");
 const path = require("path");
 
@@ -7,26 +6,14 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const SITES = path.join(__dirname, "sites");
 
-// Static sites
-app.use(vhost("yossi.eu", express.static(path.join(SITES, "yossi.eu"))));
-app.use(vhost("toma.yossi.eu", express.static(path.join(SITES, "toma.yossi.eu"))));
-app.use(vhost("correa.yossi.eu", express.static(path.join(SITES, "correa.yossi.eu"))));
+// Subsites served under /toma, /correa, /files
+app.use("/toma", express.static(path.join(SITES, "toma.yossi.eu")));
+app.use("/correa", express.static(path.join(SITES, "correa.yossi.eu")));
+app.use("/files", express.static(path.join(SITES, "files.yossi.eu")));
+app.use("/files", serveIndex(path.join(SITES, "files.yossi.eu"), { icons: true }));
 
-// File server with directory listing
-const filesApp = express();
-filesApp.use(express.static(path.join(SITES, "files.yossi.eu")));
-filesApp.use(serveIndex(path.join(SITES, "files.yossi.eu"), { icons: true }));
-app.use(vhost("files.yossi.eu", filesApp));
-
-// Redirects
-app.use(vhost("www.yossi.eu", (req, res) => {
-  res.redirect(301, `https://yossi.eu${req.url}`);
-}));
-
-// Debug: show what Host header Express is receiving
-app.use((req, res) => {
-  res.type("text").send(`No vhost matched. Host header: "${req.hostname}" (full: "${req.headers.host}")`);
-});
+// Main site at root
+app.use(express.static(path.join(SITES, "yossi.eu")));
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
