733acca21fc8131e912ab7a2b189c832c4dae707
Yossi Bokor Bleile <yossi@yossi.eu>
2026-02-12 08:51:16 +0100

    Fixing bugs?

--- server.js
+++ server.js
@@ -4,33 +4,14 @@ const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-const SITES = path.join(__dirname, "sites");
+const ROOT = path.join(__dirname, "sites", "yossi.eu");
 
-// Route subdomains to their site directories
-app.use((req, res, next) => {
-  const host = req.hostname;
-  const siteDir = path.join(SITES, host);
-  if (host !== "yossi.eu" && host.endsWith(".yossi.eu")) {
-    req.siteDir = siteDir;
-  }
-  next();
-});
+// Directory listing for /files
+app.use("/files", express.static(path.join(ROOT, "files")));
+app.use("/files", serveIndex(path.join(ROOT, "files"), { icons: true }));
 
-// File server with directory listing
-app.use((req, res, next) => {
-  if (req.hostname === "files.yossi.eu") {
-    express.static(path.join(SITES, "files.yossi.eu"))(req, res, () => {
-      serveIndex(path.join(SITES, "files.yossi.eu"), { icons: true })(req, res, next);
-    });
-  } else if (req.siteDir) {
-    express.static(req.siteDir)(req, res, next);
-  } else {
-    next();
-  }
-});
-
-// Main site at root (yossi.eu and preview URL fallback)
-app.use(express.static(path.join(SITES, "yossi.eu")));
+// Main site
+app.use(express.static(ROOT));
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
