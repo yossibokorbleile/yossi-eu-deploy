1eaff76d422bf9ed89c486ebb893eb9a5a700de6
Yossi Bokor Bleile <yossi@yossi.eu>
2026-02-12 09:27:55 +0100

    subdomains currently broken

--- server.js
+++ server.js
@@ -6,6 +6,25 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 const ROOT = path.join(__dirname, "sites", "yossi.eu");
 
+// Subdomain redirects
+const REDIRECTS = {
+  "toma.yossi.eu": "/toma",
+  "correa.yossi.eu": "/correa",
+  "files.yossi.eu": "/files",
+  "www.yossi.eu": "/",
+  "zoom.yossi.eu": "https://uni-sydney.zoom.us/j/6465140162?pwd=SPgSrZcRMLCdaZ4sQTokqbectxtVQ7.1",
+  "linkedin.yossi.eu": "https://www.linkedin.com/in/yossi-bokor-bleile",
+};
+
+app.use((req, res, next) => {
+  const target = REDIRECTS[req.hostname];
+  if (target) {
+    const url = target.startsWith("https://") ? target : `https://yossi.eu${target}`;
+    return res.redirect(301, url);
+  }
+  next();
+});
+
 // Directory listing for /files
 app.use("/files", express.static(path.join(ROOT, "files")));
 app.use("/files", serveIndex(path.join(ROOT, "files"), { icons: true }));
